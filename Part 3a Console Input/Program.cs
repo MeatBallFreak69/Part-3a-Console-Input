e28a1790729639fdd4cc737f6d912d3b225aec66
Clark Miller <glitchclick@gmail.com>
2021-02-17 12:16:38 -0500

    Add project files.

---
+++ Part 3a Console Input/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_3a_Console_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int year = DateTime.Now.Year;
+
+            Console.WriteLine("Hello, what is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("What is your age?");
+            
+            int age = Convert.ToInt32(Console.ReadLine());
+  
+            Console.WriteLine($"Your name is {name}");
+            Console.WriteLine($"Your age is {age}");
+            Console.WriteLine($"The year is {year}");
+
+
+
+
+
+
+        }
+    }
+}
